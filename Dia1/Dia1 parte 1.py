0401b74b8eb2144d0ccdc69ca24da692183b6194
JoseT <jtocroj094@g.educaand.es>
2021-12-01 14:25:10 +0100

    Añadido comentario al código

--- Dia1/Dia1 parte 1.py
+++ Dia1/Dia1 parte 1.py
@@ -1,15 +1,36 @@
 def arriba_abajo(lista):
+    """
+    [Función que recibe la lista con los inputs y compara cada uno de los valores recorridos con su anterior,
+    si es mayor se añade 1 a la suma y si no lo es se sigue con el proceso]
+    Recibe:
+        lista: Lista con los inputs del reto
+
+    Devuelve:
+        suma: Un número entero que sacamos tras revisar cuantas veces el submarino asciende
+    """
     suma = 0
     for x in range(len(lista)):
         if lista[x] > lista[x-1]:
             suma += 1
         else:
             continue
-    print(suma)
+    return suma
 
 
-if __name__ == "__main__":
+def main():
+    """
+    [Función principal del programa que lee el archivo input.txt y convierte los datos en una lista que pasa más
+    adelante a la función arriba_abajo]
+
+    Devuelve:
+        print: número de veces que el submarino asciende gracias a la función arriba_abajo
+
+    """
     with open("input.txt") as f:
         listainputs = [int(line) for line in f]
 
-    arriba_abajo(listainputs)
+    print(arriba_abajo(listainputs))
+
+
+if __name__ == "__main__":
+    main()
--- Dia1/Dia1 parte2.py
+++ Dia1/Dia1 parte2.py
@@ -1,4 +1,14 @@
 def arriba_abajo_tres(lista):
+    """
+    [Función que recibe la lista con los inputs y primero se usa un for para sumar los elementos de la lista de tres en
+    tres como se indica en el reto para después con otro for hacer la comprobación elemento a elemento y ver cuales son
+    mayores a su elemento predecesor]
+    Args:
+        lista: Lista con los inputs del reto
+
+    Returns:
+        suma: Número entero con las veces que un grupo de tres asciende más que el anterior
+    """
     suma = 0
     listatres = []
     for x in range(len(lista) - 2):
@@ -12,8 +22,20 @@ def arriba_abajo_tres(lista):
     return suma
 
 
-if __name__ == "__main__":
+def main():
+    """
+        [Función principal del programa que lee el archivo input.txt y convierte los datos en una lista que pasa más
+        adelante a la función arriba_abajo]
+
+        Devuelve:
+            print: número de veces que el submarino asciende gracias a la función arriba_abajo
+
+    """
     with open("input.txt") as f:
         listainputs = [int(line) for line in f]
 
     print(arriba_abajo_tres(listainputs))
+
+
+if __name__ == "__main__":
+    main()
